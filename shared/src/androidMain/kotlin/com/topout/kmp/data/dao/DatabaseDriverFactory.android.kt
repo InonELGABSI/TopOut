31841768484383f81df61e798b4aa6f8f0bc8211
inonELGABSI <inon11221@gmail.com>
2025-07-14 17:14:05 +0300

    connect live session screen, almost done

--- shared/src/androidMain/kotlin/com/topout/kmp/data/dao/DatabaseDriverFactory.android.kt
+++ shared/src/androidMain/kotlin/com/topout/kmp/data/dao/DatabaseDriverFactory.android.kt
@@ -8,10 +8,11 @@ actual class DatabaseDriverFactory(
     private val context: Context
 ) {
     actual fun createDriver(): SqlDriver {
+        // Use a completely new database name to force a fresh database creation
         return AndroidSqliteDriver (
             schema = AppDatabase.Schema,
             context = context,
-            name = "sessions.db"
+            name = "topout_db_v3.db"
         )
     }
 }
